27c6246514f7fddc4b8eb27a42848e37af247dee
abhiav-spec <kumarabhinav6649@gmail.com>
2026-05-04 23:34:07 +0530

    notes page completd

---
+++ frontend/src/pages/Notes.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PagePlaceholder from './PagePlaceholder';
+
+const Notes: React.FC = () => {
+  return <PagePlaceholder title="Notes" icon="description" />;
+};
+
+export default Notes;
