697eb11b90f3c823f17b34495db9d8e8afbb03c0
abhiav-spec <kumarabhinav6649@gmail.com>
2026-05-04 23:31:41 +0530

    history page is completed

---
+++ frontend/src/pages/History.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PagePlaceholder from './PagePlaceholder';
+
+const History: React.FC = () => {
+  return <PagePlaceholder title="History" icon="history" />;
+};
+
+export default History;
